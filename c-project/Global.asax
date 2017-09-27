62887a9f4833ca5ffda02e261f31b83e2a98e349
geelee999 <granthatcher52@gmail.com>
2017-09-27 18:39:28 -0400

    Add project files.

---
+++ c-project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="c_project.MvcApplication" Language="C#" %>
---
+++ c-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(c_project.Startup))]
+namespace c_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
